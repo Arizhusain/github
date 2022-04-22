d1492de3eaeae60dfe7631682cb3ef71e229aa81
Arizhusain <arifhusainsoudagar01@gmail.com>
2022-04-22 17:19:11 +0530

    remove console

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ButtonContainer from './Components/ButtonContainer'
 const App = () => {
   
   const [colorData, setColorData] = useState("");
-  console.log(colorData)
+
   return (
     <div style={{backgroundColor:`${colorData}`, height:'100vh', width:'100vw'}}>
       <ButtonContainer setColorData={setColorData}/>
